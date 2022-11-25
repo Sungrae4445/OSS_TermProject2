6f36807726129c5442315e895d90de1a5d05b1dc
kksyrr <kksyrr@gmail.com>
2022-11-25 22:42:21 +0900

    implement split_dataset

--- template.py
+++ template.py
@@ -4,6 +4,7 @@
 import sys
 import pandas as pd
 import sklearn 
+from sklearn.model_selection import train_test_split
 
 def load_dataset(dataset_path):
 	return pd.read_csv(dataset_path)
@@ -15,9 +16,14 @@ def dataset_stat(dataset_df):
 	class_1_count = len(class_df[class_df==1])
 	return num_features,class_0_count,class_1_count
 
-# 
-# def decision_tree_train_test(x_train, x_test, y_train, y_test):
-# 	#To-Do: Implement this function
+def split_dataset(dataset_df, testset_size):
+    X=dataset_df.drop(columns="target",axis=1)
+    y=dataset_df['target']
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=testset_size,random_state=2)
+    return X_train,X_test,y_train,y_test
+
+def decision_tree_train_test(x_train, x_test, y_train, y_test):
+	#To-Do: Implement this function
 
 # def random_forest_train_test(x_train, x_test, y_train, y_test):
 # 	#To-Do: Implement this function
@@ -25,11 +31,11 @@ def dataset_stat(dataset_df):
 # def svm_train_test(x_train, x_test, y_train, y_test):
 # 	#To-Do: Implement this function
 
-# def print_performances(acc, prec, recall):
-# 	#Do not modify this function!
-# 	print ("Accuracy: ", acc)
-# 	print ("Precision: ", prec)
-# 	print ("Recall: ", recall)
+def print_performances(acc, prec, recall):
+	#Do not modify this function!
+	print ("Accuracy: ", acc)
+	print ("Precision: ", prec)
+	print ("Recall: ", recall)
 
 if __name__ == '__main__':
 	#Do not modify the main script!
@@ -41,17 +47,17 @@ if __name__ == '__main__':
 	print ("Number of class 0 data entries: ", n_class0)
 	print ("Number of class 1 data entries: ", n_class1)
 
-	# print ("\nSplitting the dataset with the test size of ", float(sys.argv[2]))
-	# x_train, x_test, y_train, y_test = split_dataset(data_df, float(sys.argv[2]))
+	print ("\nSplitting the dataset with the test size of ", float(sys.argv[2]))
+	x_train, x_test, y_train, y_test = split_dataset(data_df, float(sys.argv[2]))
 
-	# acc, prec, recall = decision_tree_train_test(x_train, x_test, y_train, y_test)
-	# print ("\nDecision Tree Performances")
-	# print_performances(acc, prec, recall)
+	acc, prec, recall = decision_tree_train_test(x_train, x_test, y_train, y_test)
+	print ("\nDecision Tree Performances")
+	print_performances(acc, prec, recall)
 
-	# acc, prec, recall = random_forest_train_test(x_train, x_test, y_train, y_test)
-	# print ("\nRandom Forest Performances")
-	# print_performances(acc, prec, recall)
+	acc, prec, recall = random_forest_train_test(x_train, x_test, y_train, y_test)
+	print ("\nRandom Forest Performances")
+	print_performances(acc, prec, recall)
 
-	# acc, prec, recall = svm_train_test(x_train, x_test, y_train, y_test)
-	# print ("\nSVM Performances")
-	# print_performances(acc, prec, recall)
+	acc, prec, recall = svm_train_test(x_train, x_test, y_train, y_test)
+	print ("\nSVM Performances")
+	print_performances(acc, prec, recall)
